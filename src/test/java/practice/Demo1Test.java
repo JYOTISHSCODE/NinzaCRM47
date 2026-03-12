c771cc3ffedda517fa2a78a639b3acf62a3e2bc9
NAYAKSCODE <anashutoshnayak@gmail.com>
2026-03-12 07:51:54 +0530

    first time commiting

---
+++ src/test/java/practice/Demo1Test.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+  @Test(groups="Regression")
+  public void carTest() {
+	  
+	  System.out.println("BMW");
+  }
+  
+}
---
+++ src/test/java/practice/DemoTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @Test(groups="smoke")
+    public void bikeTest() {
+    	
+    	System.out.println("RE");
+    }
+}
